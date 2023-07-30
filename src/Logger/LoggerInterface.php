302bf7616a1e878daabf56b8d5bb0445b9ac6f5b
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2023-07-30 21:52:26 +0400

    logging

---
+++ src/Logger/LoggerInterface.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types=1);
+
+namespace FurqanSiddiqui\WebSocket\Logger;
+
+use FurqanSiddiqui\WebSocket\Server\User;
+
+/**
+ * Interface LoggerInterface
+ * @package FurqanSiddiqui\WebSocket\Logger
+ */
+interface LoggerInterface
+{
+    public function exception(\Throwable $t);
+
+    public function connectionReceived(string $ip, int $port): void;
+
+    public function connectionLost(string $ip, int $port): void;
+
+    public function connectionTerminated(string $ip, int $port, int $code, ?string $message = null): void;
+
+    public function connectionAccepted(User $user): void;
+
+    public function disconnect(User $user, int $code, string $message): void;
+
+}
